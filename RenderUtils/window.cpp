4be8596525a831397e111bda5b04e97caef7a052
braiden.braaten <braiden.braaten@students.aie.edu.au>
2016-08-25 16:10:19 -0700

    added a debug feature

--- RenderUtils/window.cpp
+++ RenderUtils/window.cpp
@@ -1,6 +1,26 @@
 #include "gldecs.h"
 #include "window.h"
 #include <cstring>
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef _DEBUG
+
+
+
+void APIENTRY GL_errorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
+{
+	//c++ equiv = std::cerr << message << std::endl;
+	fprintf(stderr, "%s\n", message);
+
+	if (severity == GL_DEBUG_SEVERITY_HIGH)
+	{
+		abort();
+	}
+}
+
+#endif
+
 
 bool Window::init(int a_width, int a_height, char * a_title)
 {
@@ -19,6 +39,17 @@ bool Window::init(int a_width, int a_height, char * a_title)
 	glewExperimental = true;
 	glewInit();
 	glClearColor(0.0f, 255.0f, 255.0f, 1.0f);
+
+#ifdef _DEBUG
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+	glDebugMessageCallback(GL_errorCallback, NULL);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, true);
+
+#endif
+
 
 	return true;
 }
