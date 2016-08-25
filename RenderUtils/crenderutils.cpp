5901c23ed3dfa6b571ac4a5fe35887f84cc3cdf9
braiden.braaten <braiden.braaten@students.aie.edu.au>
2016-08-25 13:04:46 -0700

    Started on talking through openGL to make geometry and free geometry

--- RenderUtils/crenderutils.cpp
+++ RenderUtils/crenderutils.cpp
@@ -1,13 +1,47 @@
 #define GLEW_STATIC
 
 #include "GLEW\glew.h"
-#include "GLFW\glfw3.h"
-
+#include "Vertex.h"
 #include "crenderutils.h"
 
-
-void test()
+Geometry makeGeometry(Vertex * verts, size_t vsize, unsigned int * tris, size_t tsize)
 {
-	glfwInit();
-	glfwTerminate();
+	Geometry retval;
+	retval.size = tsize;
+	//define the variables
+	glCreateBuffers(1, &retval.vbo); //store my vertices
+	glCreateBuffers(1, &retval.ibo); //store my indices
+	glCreateVertexArrays(1, &retval.vao); //store attribute information
+
+	// scope the variables
+	glBindVertexArray(retval.vao); 
+	glBindBuffer(GL_ARRAY_BUFFER, retval.vbo); //scope our vertices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, retval.ibo); // triangle is scoped
+
+	//initialize the variables
+	glBufferData(GL_ARRAY_BUFFER        , vsize * sizeof(Vertex)  , verts, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, tsize * sizeof(unsigned), tris , GL_STATIC_DRAW);
+
+	// Attributes let us tell openGL how memory is laid out
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	
+	// index of the attribute, number of elements, type, normalized?, size of vertex, offset
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)16);
+
+	//unscope the variables
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER        , 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	return retval;
+}
+
+void freeGeometry(Geometry &geo)
+{
+	glDeleteBuffers(1, &geo.vbo);
+	glDeleteBuffers(1, &geo.ibo);
+	glDeleteVertexArrays(1, &geo.vao);
+	geo = { 0,0,0,0 };
 }
--- RenderUtils/crenderutils.h
+++ RenderUtils/crenderutils.h
@@ -1,2 +1,18 @@
 #pragma once
-void test();
+
+
+
+struct Geometry
+{
+	unsigned vbo, ibo, vao, size;
+	// Vertex Buffer Object : an array of vertices
+	// Index Buffer Object  : an array of indices (triangles)
+	// Vertex Array Object  : Groups of two with some formatting
+	// size                 : number of triangles
+
+};
+
+Geometry makeGeometry(struct Vertex *verts, size_t vsize, 
+		                unsigned int *tris, size_t tsize);
+
+void freeGeometry(Geometry &);
